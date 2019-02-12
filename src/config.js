160076c964660c27f4af12f3a8baa23e5699deb3
pngwn <hello@pngwn.io>
2019-02-12 10:39:04 +0000

    Use HTTPS API in production.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
-export const baseUrl = 'http://3.8.107.150:9200/rwjf_test/_search';
+export const baseUrl = dev
+  ? 'http://3.8.107.150:9200/rwjf_test/_search'
+  : 'https://18.130.95.111:9200/rwjf_test/_search';
 export const size = dev ? 200 : 1000;
