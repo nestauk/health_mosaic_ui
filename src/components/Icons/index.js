f5a852e1e5bc722d357f2a036bd03f5969f1b438
pngwn <hello@pngwn.io>
2019-03-04 17:59:43 +0000

    Implement working map UI with notes and edit controls. Closes #54

---
+++ src/components/Icons/index.js
@@ -0,0 +1,8 @@
+export { default as Add } from './Add.html';
+export { default as Alert } from './Alert.html';
+export { default as Close } from './Close.html';
+export { default as Delete } from './Delete.html';
+export { default as Edit } from './Edit.html';
+export { default as Pin } from './Pin.html';
+export { default as Save } from './Save.html';
+export { default as Undo } from './Undo.html';
---
+++ src/stores/memo.ts
@@ -0,0 +1,32 @@
+import { writable } from 'svelte/store';
+
+type bounds = [[number, number], [number, number]];
+
+/**
+ * An individual pin
+ *
+ * @interface
+ *
+ * @property {number} id- A unique identifier for this pin.
+ * @property {string} title - A name for this pin.
+ * @property {string} description - A name for this pin.
+ * @property {array} bounds - The geographic bounds for this pin: [swBound,  neBounds]
+ */
+interface Pin {
+  id: number;
+  title: string;
+  description: string;
+  bounds: bounds;
+}
+
+interface Memo {
+  pins: Pin[];
+  currentPin: number | boolean;
+}
+
+const initialValue: Memo = {
+  pins: [],
+  currentPin: false,
+};
+
+export const createMemoStore = () => writable(initialValue);
