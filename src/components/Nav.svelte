7cfaca546892cfa70e6ba13eae8826068b9ba9af
pngwn <hello@pngwn.io>
2019-07-22 15:38:24 +0100

    Implement tab scrolling and related behaviours. Closes #198.

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,19 +1,38 @@
 <script>
-  import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher, tick, onMount } from 'svelte';
   import Spinner from './Spinner.svelte';
   import Alert from './Icons/Alert.svelte';
 
-  import closeIcon from 'ionicons/dist/ionicons/svg/ios-close-circle-outline.svg';
   import addIcon from 'ionicons/dist/ionicons/svg/ios-add-circle-outline.svg';
+  import arrowForward from 'ionicons/dist/ionicons/svg/ios-arrow-forward.svg';
+  import closeIcon from 'ionicons/dist/ionicons/svg/ios-close-circle-outline.svg';
 
-  export let tabs,
-    activeTab,
-    tabHeight = 0,
-    isLoading,
-    isError;
+  export let activeTab;
+  export let isError;
+  export let isLoading;
+  export let tabHeight = 0;
+  export let tabs;
+
+  let editedTarget = null;
+  let isFullWidth = false;
+  let isLeft;
+  let isRight;
+  let navEl;
+  let tabEls = [];
+  let tabWidth;
+  let windowWidth;
 
   const dispatch = createEventDispatcher();
-  let editedTarget = null;
+
+  const calculateWidth = async () => {
+    await tick();
+    tabEls = tabEls.filter(v => !!v);
+    tabWidth = [].reduce.call(tabEls, (a, n) => a + n.offsetWidth, 0)
+    // accounts for margins, etc.
+    isFullWidth =  tabWidth + 123 > windowWidth;
+    isRight = (tabWidth - navEl.offsetWidth - navEl.scrollLeft) === 0;
+    isLeft = navEl.scrollLeft === 0;
+  }
 
   const handleDoubleClick = ({ target }) => {
     if (editedTarget) return;
@@ -57,19 +76,52 @@
       editedTarget = null;
     }
   };
+
+  const newTab = async () => {
+    dispatch('newtab')
+    await tick();
+    tabEls[tabEls.length - 1].scrollIntoView();
+    calculateWidth();
+  }
+
+  const textChange = ({ target }, id) => {
+    dispatch('textchange', { value: target.innerText, id });
+    calculateWidth();
+  }
+
+  const deleteTab = async id => {
+    dispatch('deletetab', id)
+    calculateWidth();
+  }
+
+  onMount(calculateWidth)
 </script>
 
-<svelte:window on:click|stopPropagation="{stopEdit}" />
+<svelte:window
+  on:click|stopPropagation="{stopEdit}"
+  bind:innerWidth={windowWidth}
+/>
 
 <nav bind:offsetHeight="{tabHeight}">
-  <ul>
-    {#each tabs as [id, tab]}
-      <li>
+  {#if isFullWidth}
+    <div
+      class="scroll scroll-left"
+      style="opacity:{isLeft ? 0.3 : 1}"
+      on:click={() => navEl.scrollLeft -= 100}
+    >
+      <span class="icon">
+        <img alt="scroll tabs left" src="{arrowForward}"/>
+      </span>
+    </div>
+  {/if}
+  <ul bind:this={navEl} on:scroll={calculateWidth}>
+    {#each tabs as [id, tab], i}
+      <li bind:this={tabEls[i]}>
         <div
           on:click|preventDefault="{() => dispatch('changetab', id)}"
           on:dblclick="{handleDoubleClick}"
           on:keydown="{stopEdit}"
-          on:input="{({ target }) => dispatch('textchange', { value: target.innerText, id })}"
+          on:input="{ e => textChange(e, id) }"
           class:selected="{parseInt(id, 10) === activeTab}"
           class="button"
         >
@@ -77,26 +129,42 @@
         </div>
 
         {#if tabs.length > 1}
-        <span on:click="{() => dispatch('deletetab', id)}" class="remove">
+        <span on:click="{() => deleteTab(id)}" class="icon">
           <img alt="delete tab" src="{closeIcon}" />
         </span>
         {/if}
       </li>
     {/each}
-    <li class="close-container">
-      <span on:click="{() => dispatch('newtab')}" class="newtab remove">
-        <img alt="create tab" src="{addIcon}"/>
-      </span>
-    </li>
+
   </ul>
-  {#if isLoading }
+  {#if isFullWidth}
+    <div
+      class="scroll scroll-right"
+      style="opacity:{isRight ? 0.3 : 1}"
+      on:click={() => navEl.scrollLeft += 100}
+    >
+      <span class="icon">
+        <img alt="scroll tabs left" src="{arrowForward}"/>
+      </span>
+    </div>
+  {/if}
+  <div
+    class="close-container"
+    class:sticky={isFullWidth}
+    style="left:{isFullWidth ? 'unset' : `${tabWidth + 30}px`}"
+  >
+    <span on:click="{newTab}" class="newtab icon">
+      <img alt="create tab" src="{addIcon}"/>
+    </span>
+  </div>
+  {#if isLoading}
     <span><Spinner/></span>
   {:else if isError}
     <span class="error"><Alert color="red"/></span>
   {/if}
 </nav>
 
-<style>
+<style lang="less">
   span {
     height: 3.5rem;
     margin-top: -0.25rem;
@@ -111,7 +179,7 @@
   nav {
     border-bottom: 1px solid rgba(170, 30, 30, 0.1);
     font-weight: 300;
-    padding: 0 1em;
+    padding: 0 30px;
     display: flex;
     justify-content: space-between;
     background: #fff;
@@ -125,6 +193,12 @@
     margin: 0;
     padding: 0;
     list-style: none;
+    display: flex;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    position: relative;
+    margin-right: 64px;
+    padding-right: 64px;
   }
 
   /* clearfix */
@@ -140,6 +214,7 @@
     align-items: center;
     border-right: 1px solid #ccc;
     padding: 0 10px;
+    flex-shrink: 0;
   }
 
   .selected {
@@ -167,7 +242,7 @@
     font-weight: 200;
   }
 
-  .remove {
+  .icon {
     height: 22px;
     width: 22px;
     cursor: pointer;
@@ -177,27 +252,67 @@
     flex-direction: column;
     justify-content: center;
     align-items: center;
-  }
-
-  .remove:hover {
-    opacity: 1;
-  }
-
-  .newtab {
     margin: auto;
     opacity: 1;
+
+    &:hover {
+      opacity: 1;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
 
+
+  .scroll {
+    position: fixed;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    border-right: none;
+    height: 61px;
+    height: 61px;
+    width: 45px;
+    border-left: 1px solid #ccc;
+    background: #fff;
+    z-index: 2;
+
+    &.scroll-left {
+      transform: rotate(180deg);
+    }
+
+    &.scroll-right {
+      left: unset;
+      right: 50px;
+    }
+  }
+
+
+
   .close-container {
+    flex-direction: column;
+    display: flex;
     flex-direction: column;
     justify-content: center;
     height: 100%;
     margin-left: 10px;
     border-right: none;
+    position:fixed;
+    height: 61px;
+    //width: 60px;
+
+    &.sticky {
+      position: fixed;
+      right: 0;
+      top: 0;
+      width: 50px;
+      background: #fff;
+      border-left: 1px solid #ccc;
+    }
   }
 
-  .remove img {
-    width: 100%;
-    height: 100%;
-  }
 </style>
