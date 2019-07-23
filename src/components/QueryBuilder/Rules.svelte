791fd1cc2c3d834f2b1dbbe2c9a8a9595f4cd1bb
pngwn <hello@pngwn.io>
2019-07-23 17:42:07 +0100

    Add arrows to notify users of the ability to scroll.

--- src/components/QueryBuilder/Rules.svelte
+++ src/components/QueryBuilder/Rules.svelte
@@ -1,13 +1,54 @@
 <script>
+  import arrowForward from 'ionicons/dist/ionicons/svg/ios-arrow-forward.svg';
   export let selectionsActive = false;
+  let width;
+
 </script>
 
-<ul class="rules-container" class:compact={!selectionsActive}>
-  <slot></slot>
-</ul>
+<svelte:window bind:innerWidth={width}/>
 
+<div class="container">
+  <div class="icon left">
+    <img src={arrowForward} >
+  </div>
+  <ul
+    class="rules-container"
+    style="width:{selectionsActive ? '100%' : `${width - 600}px`}"
+  >
+    <slot></slot>
+  </ul>
+  <div class="icon right" style="right:{selectionsActive ? -30 : 470 }px">
+    <img src={arrowForward} >
+</div>
+</div>
 
 <style lang="less">
+  .icon {
+    height: 30px;
+    position: absolute;
+    height: 30px;
+    top: 40px;
+    position: absolute;
+    bottom: 0;
+    margin: auto;
+
+    &.left {
+      transform: rotate(180deg);
+      left: -30px;
+    }
+
+    &.right {
+      left: unset;
+    }
+
+    img {
+      height: 100%;
+    }
+  }
+  .container {
+    position: relative;
+    width: 100%;
+  }
   .rules-container {
     display: flex;
     transition: 0.2s 0.2s;
@@ -18,9 +59,9 @@
     overflow-x: scroll;
     list-style: none;
 
-    &.compact {
+    /* &.compact {
       width: calc(100% - 500px);
-    }
+    } */
   }
 
 </style>
