e8e745183f38827ee246779221e14ca7fbf2d249
pngwn <hello@pngwn.io>
2019-08-20 18:03:16 +0100

    Begin adding query pills and search input.

--- src/components/Search/RulesetQueries.svelte
+++ src/components/Search/RulesetQueries.svelte
@@ -1,15 +1,78 @@
 <script>
   import { Edit } from '../Icons';
   export let isEditing = true;
+  export let queries = [{ query: 'Heart', status: 'include' }, { query: 'Disease', status: 'exclude' }];
+
+  $: textQuery = queries.map(({ query, status }) => (status === 'exclude' ? '-' : '') + query, "").join(', ');
 </script>
 
-<div class="input">
-  <input />
-  <button><Edit /></button>
-</div>
+{#if !isEditing}
+  <div class="no-edit">
+    <div class="query-labels">
+      <ul>
+        {#each queries as { query, status }}
+          <li class={status}>{query}</li>
+        {/each}
+      </ul>
+    </div>
+    <button on:click={() => isEditing = true}><Edit /></button>
+  </div>
+{:else}
+  <div class="edit">
+    <input value={textQuery}/>
+    <button on:click={() => isEditing = false}>x</button>
+  </div>
+{/if}
 
 <style lang="less">
-  .input {
+  .no-edit {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 15px;
+
+    button {
+      padding: 0;
+      width: 30px;
+      height: 30px;
+      border: none;
+      cursor: pointer;
+      color: #999;
+
+      &:inactive {
+        opacity: 0.6;
+      }
+
+      &:hover {
+        color: #333;
+      }
+    }
+
+    ul {
+      display: flex;
+      padding: 3px 5px;
+      margin: 0;
+      list-style: none;
+
+      li {
+        padding: 3px 9px;
+        border: 1px solid #ccc;
+        margin: 5px 5px 0 0;
+        border-radius: 3px;
+
+        &.include {
+          color: green;
+          border-color: green;
+        }
+
+        &.exclude {
+          color: red;
+          border-color: red;
+        }
+      }
+    }
+  }
+
+  .edit {
     display: flex;
     justify-content: space-between;
 
@@ -20,6 +83,8 @@
       border: none;
       cursor: pointer;
       color: #999;
+      transform: translateY(5px);
+
       &:inactive {
         opacity: 0.6;
       }
@@ -39,6 +104,7 @@
     background: none;
     padding: 5px;
     margin: 0 20px 15px 0;
+    padding: 14px 9px 8px;
 
     &:focus {
       border-color: #333;
