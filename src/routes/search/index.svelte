5cf19c349a5d92d18888ea108c339982a29573b8
pngwn <hello@pngwn.io>
2019-08-30 13:09:49 +0100

    Remove uneeded styles from search index.

--- src/routes/search/index.svelte
+++ src/routes/search/index.svelte
@@ -20,58 +20,5 @@
     height: 100%;
     display: flex;
     flex-direction: column;
-
-    .header {
-      padding: 0.5em;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      border-bottom: 1px solid lightgrey;
-      .buttons {
-        width: 56px;
-        display: grid;
-        height: 25px;
-        grid-template-columns: 1fr 1fr;
-        span {
-          opacity: 0.6;
-          cursor: pointer;
-          &:hover {
-            opacity: 1;
-          }
-        }
-      }
-    }
-
-    ul {
-      display: flex;
-      list-style: none;
-
-      li {
-        margin-left: 2.4em;
-        position: relative;
-
-        &::before {
-          content: '';
-          position: absolute;
-          width: 10px;
-          height: 10px;
-          border-radius: 5px;
-          left: -20px;
-          top: 7px
-        }
-
-        &.company::before {
-          background: var(--color-type-company);
-        }
-
-        &.meetup::before {
-          background: var(--color-type-event);
-        }
-
-        &.paper::before {
-          background: var(--color-type-paper);
-        }
-      }
-    }
   }
 </style>
